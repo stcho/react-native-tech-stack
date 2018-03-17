c4d6de7dead2541ef38aa4e32ebc4438eede7551
Thomas Cho <s.thomas.cho@gmail.com>
2018-03-16 23:30:24 -0700

    Refactor ListView to FlatList

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,33 +1,27 @@
 import React, { Component } from 'react';
-import { ListView } from 'react-native';
 import { connect } from 'react-redux';
+import { FlatList } from 'react-native';
 import ListItem from './ListItem';
-
+ 
 class LibraryList extends Component {
-  componentWillMount() {
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2
-    });
-
-    this.dataSource = ds.cloneWithRows(this.props.libraries);
+ 
+  renderItem({ item }) {
+    return <ListItem item={item} />;
   }
-
-  renderRow(library) {
-    return <ListItem library={library} />;
-  }
-
+ 
   render() {
     return (
-      <ListView
-        dataSource={this.dataSource}
-        renderRow={this.renderRow}
+      <FlatList
+        data={this.props.libraries}
+        renderItem={this.renderItem}
+        keyExtractor={(item) => item.id.toString()}
       />
     );
   }
 }
-
+ 
 const mapStateToProps = state => {
   return { libraries: state.libraries };
 };
-
+ 
 export default connect(mapStateToProps)(LibraryList);
